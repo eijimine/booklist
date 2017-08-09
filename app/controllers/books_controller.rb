2d3591781a3b6b9a8dae55b16155db7cc98b916c
eiji <eijimine@gmail.com>
2017-08-09 14:30:41 -0400

    Added respond_to

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,20 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      #
+      # format.json do
+      #   render json: @monsters.map { |monster|
+      #     {name: monster.name, home: monster.home, creepiness: monster.creepiness}
+      #   }
+      # end
+
+    end
+
+  end
 
 end
