d28815d07f2c0e66a0bbec4000aca5d8291fb411
eiji <eijimine@gmail.com>
2017-08-09 15:07:15 -0400

    Assignment Complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,21 @@ class BooksController < ApplicationController
     respond_to do |format|
       format.html
       format.text
-      #
-      # format.json do
-      #   render json: @monsters.map { |monster|
-      #     {name: monster.name, home: monster.home, creepiness: monster.creepiness}
-      #   }
-      # end
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books
+        # .map { |book|
+          # {Title: book.title, Author: book.author, Already_Read: book.already_read}
+        # }
+      end
 
     end
 
   end
+
 
 end
